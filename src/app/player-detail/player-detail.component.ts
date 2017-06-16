555540e9023252bcf5777930d69adc7ce36163ef
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-06-16 11:54:56 -0700

    add service

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-player-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-detail.component.css']
 })
 export class PlayerDetailComponent implements OnInit {
+  playerId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.playerId = parseInt(urlParameters['id'])
+    })
   }
 
 }
---
+++ src/app/player.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+
+@Injectable()
+export class PlayerService {
+
+  constructor() { }
+
+}
